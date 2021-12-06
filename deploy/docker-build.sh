7460277b726553f9c992f376ce49fa5dc1d3facb
smartbooks <smartbooks@qq.com>
2021-12-06 13:26:13 +0800

    auto commit

--- deploy/docker-build.sh
+++ deploy/docker-build.sh
@@ -4,6 +4,19 @@ export cur_home=$(cd `dirname $0`; pwd)
 
 cd ${cur_home}
 
+function docker_build_base(){
+  dockerfile="${cur_home}/Dockerfile.base"
+  image="ubuntu1804-java8"
+  version="1.0.0"
+
+  #docker build -t smartbooks/ubuntu1804-java8:1.0.0 -f Dockerfile.base .
+  #docker build -t <hub-user>/<repo-name>[:<tag>]
+  #docker push <hub-user>/<repo-name>:<tag>
+
+  docker build -t ${image}:${version} -f ${dockerfile} ../
+  docker push ${image}:${version}
+}
+
 function docker_build_admin(){
   dockerfile="${cur_home}/Dockerfile.admin"
   image="tensquare_admin_service"
